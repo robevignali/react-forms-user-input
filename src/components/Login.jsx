79a2ac9dae9f0ea7c5f35f6dda5861beeee1b06c
robevignali <robevignali@gmail.com>
2024-12-09 19:36:46 +0100

    react-hook-form in Login module

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,35 +1,50 @@
-export default function Login() {
+import { useForm } from "react-hook-form";
 
-  function handleOnClick(event) {
-    event.preventDefault();
-    console.log("Clicked !");
+export default function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
+
+  function handleFetchData(data) {
+    console.log(data);
   }
 
   return (
-    <form  onClick={handleOnClick}>
+    <form  
+      onSubmit={handleSubmit((data)=>{handleFetchData(data)})}
+      >
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          {/* <input id="email" type="email" name="email" /> */}
+          <input {...register("email", {required:true})} />
+          <div className="control-error">
+            {errors.email && <p>Email error</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input {...register("password",{required:true,maxLength:8,minLength:6})} />
+          <div className="control-error">
+            {errors.password && <p>Password error</p>}
+          </div>
         </div>
       </div>
 
       <p className="form-actions">
         <button className="button button-flat">Reset</button>
         <button 
-          //type="button" 
+          type="submit" 
           className="button" 
-          //onClick={handleOnClick}
           >
             Login
         </button>
+
       </p>
     </form>
   );
